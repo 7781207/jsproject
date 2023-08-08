76b73c5a9256ba423b39c304fbd7eb4dd2090b2a
7781207 <mallulajahnavi778@gmail.com>
2023-08-09 00:10:17 +0530

    worked on delete button

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const tastcontainer=document.querySelector(".task__container");
-const globalStore=[];
+var globalStore=[];
 const localinitialcarddata=()=>{
+    //localStorage to get tasky data
     const dstask=localStorage.getItem("tasky");
+    //convert from string to normal object
     const {card}=JSON.parse(dstask);
+    //loop over the cards data and  generate new data
     card.map((tsda)=>{
         tastcontainer.insertAdjacentHTML("beforeend",generatenewcard(tsda));
         globalStore.push(tsda);
     })
 };
+//Generation of new card
+
 const generatenewcard=(tsdata)=>`
     <div class="col-md-6 col-lg-4" id=${tsdata.id}>
                         <div class="card ">
                             <div class="card-header d-flex justify-content-end gap-2">
-                                <button type="button" class="btn btn-outline-success"><i class="fa-regular fa-pencil"></i></button>
-                                <button type="button" class="btn btn-outline-danger"><i class="fa-solid fa-trash"></i></button>
+                                <button type="button" class="btn btn-outline-success" ><i class="fa-solid fa-pencil"></i></button>
+                                <button type="button" class="btn btn-outline-danger" onclick="deletecard.apply(this,arguments)"><i class="fa-solid fa-trash" onclick="deletecard.apply(this,arguments)"></i></button>
                             </div>
                             <img src=${tsdata.imageurl} class="card-img-top" alt="...">
                             <div class="card-body">
@@ -27,8 +32,9 @@ const generatenewcard=(tsdata)=>`
                         </div>
                         
                     </div>
- `;
-
+ `
+ ;
+// Savechanges onclick function
 const savechanges=()=>{
     const tsdata={
         id:`{$date.now()}`,
@@ -37,9 +43,25 @@ const savechanges=()=>{
         tasktype: document.getElementById("tasktype").value,
         taskdescrip: document.getElementById("taskdes").value,
     };
-
+     
     tastcontainer.insertAdjacentHTML("beforeend",generatenewcard(tsdata));
     globalStore.push(tsdata);
-    localStorage.setItem("tasky",JSON.stringify({card:globalStore}));
+    localStorage.setItem("tasky",JSON.stringify({card:globalStore}));// an object
 
+};
+const deletecard=(event)=>{
+    event =window.event;
+    const targetId=event.target.id;
+    const tagname=event.target.tagName;
+
+    globalStore = globalStore.filter((caect)=>caect.id !== targetId);
+
+    localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
+
+    if(tagname==="BUTTON"){
+        return tastcontainer.removeChild(event.target.parentNode.parentNode.parentNode);
+    }else{
+        return tastcontainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+    }
+    
 };
